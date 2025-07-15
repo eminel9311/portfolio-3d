8669e9f59399ca7b5f4d648cb633c161da1d95a9
quanbh-vnpt <buihongquan@vnpt.vn>
2025-07-15 18:09:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 opacity-50">Hello</div>
+    </main>
+  );
+};
+
+export default App;
